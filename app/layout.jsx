21e2f252ac33dd7f2bb51663573923b8a9422916
Ganesh Lonare <ganeshlonare311@gmail.com>
2025-09-21 22:35:43 +0530

    Update SEO configuration and metadata URLs for consistency

--- app/layout.jsx
+++ app/layout.jsx
@@ -37,7 +37,7 @@ export const metadata = {
     siteName: siteConfig.name,
     images: [
       {
-        url: siteConfig.ogImage,
+        url: `${siteConfig.url}${siteConfig.ogImage}`,
         width: 1200,
         height: 630,
         alt: siteConfig.name,
@@ -48,7 +48,7 @@ export const metadata = {
     card: "summary_large_image",
     title: siteConfig.name,
     description: siteConfig.description,
-    images: [siteConfig.ogImage],
+    images: [`${siteConfig.url}${siteConfig.ogImage}`],
     creator: siteConfig.social.twitter,
   },
   alternates: {
